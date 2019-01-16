45faef06050a1e4955d0a88a0020eac58321c5b3
chandrima04 <chandrima.004@google.com>
2019-01-16 17:03:51 -0500

    Merge request errors solved

--- effet_oppos/classification.py
+++ effet_oppos/classification.py
@@ -1,4 +1,3 @@
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
@@ -9,9 +8,9 @@ def split_data(data_tbl, features, test_size=0.2):
 
 def train_model(data_tbl, features, method='random_forest', n_estimators=20):
     """Return a trained model to predict features from data."""
-    regressor =RandomForestClassifier(n_estimators=n_estimators, random_state=0)
-    regressor.fit(data_tbl, features)
-    return regressor
+    classifier = RandomForestClassifier(n_estimators=n_estimators, random_state=0)
+    classifier.fit(data_tbl, features)
+    return classifier
 
 
 def predict_with_model(model, data_tbl):

140c9c01e47c515d9fdccb50e81d84bebc656ecb
David Danko <dcdanko@gmail.com>
2019-01-30 16:15:23 -0500

    code clean

--- effet_oppos/classification.py
+++ effet_oppos/classification.py
@@ -8,9 +8,10 @@ from sklearn import svm
 import numpy as np
 
 
-def split_data(data_tbl, features, test_size=0.2):
+def split_data(data_tbl, features, test_size=0.2, seed=None):
     """Return a tuple of length four with train data, test data, train feature, test feature."""
-    return train_test_split(data_tbl, features, test_size=test_size)
+    return train_test_split(data_tbl, features, test_size=test_size, random_state=seed)
+
 
 def train_model(data_tbl, features, method='random_forest', n_estimators=20, n_neighbours=21):
     """Return a trained model to predict features from data."""
@@ -24,29 +25,32 @@ def train_model(data_tbl, features, method='random_forest', n_estimators=20, n_n
     elif (method == "linear_svc"):
         classifier = svm.SVC(kernel='linear', probability=True)
     elif (method == "svm"):
-        classifier = svm.SVC(gamma='scale', decision_function_shape='ovo', kernel="rbf", probability=True)
+        classifier = svm.SVC(
+            gamma='scale', decision_function_shape='ovo', kernel="rbf", probability=True
+        )
     elif (method == "neural_network"):
         classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2))
     classifier.fit(data_tbl, features)
     return classifier
 
+
 def predict_with_model(model, data_tbl):
     """Return a dictionary with evaluation data for the model on the data."""
     return model.predict(data_tbl)
+
 
 def multi_predict_with_model(model, data_tbl):
     """Return a dictionary with evaluation data for all the classes of the model on the data."""
     return model.predict_proba(data_tbl)
 
-def predict_top_classes(model, data_tbl, features):
+
+def predict_top_classes(model, data_tbl, features, top_hits=[1, 2, 3, 5, 10]):
     prediction = multi_predict_with_model(model, data_tbl)
-    top_hits = [1,2,3,5,10]
     hit_values = []
     for i in top_hits:
-        top_n_hits = np.argsort(-prediction, axis=1)[:,:i]
+        top_n_hits = np.argsort(-prediction, axis=1)[:, :i]
         hits = 0
         for i, val in enumerate(features):
-            hits += 1 if val in top_n_hits[i] else 0 
-        hit_values.append(hits / len(features)) 
+            hits += 1 if val in top_n_hits[i] else 0
+        hit_values.append(hits / len(features))
     return hit_values
-
--- effet_oppos/cli.py
+++ effet_oppos/cli.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import click
-from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, classification_report, accuracy_score
-
+from itertools import product
+from random import randint
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    confusion_matrix,
+    classification_report,
+    accuracy_score,
+)
 from .preprocessing import (
     parse_raw_data,
     parse_feature,
@@ -11,76 +18,74 @@ from .classification import (
     split_data,
     train_model,
     predict_with_model,
-    multi_predict_with_model,
     predict_top_classes,
 )
 
+MODEL_NAMES = ['random_forest', 'gaussian', 'knn', 'svm', 'linear_svc', 'neural_network']
+NORMALIZER_NAMES = ['raw', 'standard_scalar', 'total_sum', 'binary']
 
-@click.command()
+
+@click.group()
+def main():
+    pass
+
+
+@main.command('one')
+def eval_one():
+    """Train and evaluate a model. Print the model results to stderr."""
+    pass
+
+
+@main.command('all')
 @click.option('--test-size', default=0.2, help='The relative size of the test data')
 @click.option('--num-estimators', default=20, help='Number of trees in our random forest')
 @click.option('--num-neighbours', default=21, help='Number of clusters in our knn')
 @click.option('--model-type', default='random_forest', help='The model type to train')
 @click.option('--feature-name', default='city', help='The feature to predict')
-@click.option('--normalize-method', default='standard_scalar', help='Normalization method.')
-@click.option('--normalize-threshold', default='0.0001', help='Normalization threshold for binary normalization.')
-@click.argument('metadata_filename')
-@click.argument('data_filename')
-def main(test_size, num_estimators, num_neighbours, model_type, feature_name, normalize_method, normalize_threshold,
-         metadata_filename, data_filename):
-    """Train and evaluate a model. Print the model results to stderr."""
-    model_name = ['random_forest', 'gaussian', 'knn', 'svm', 'linear_svc', 'neural_network']
-    normalize_model = ['raw', 'standard_scalar', 'total_sum', 'binary']
-    i = 0
-    top1, top3, top5, top10, model_classifier, model_normalization, precision, recall = [],[], [], [], [], [], [], []
-    for i in range(len(model_name)):
-        j = 0
-        for j in range(len(normalize_model)):
-            model_type = model_name[i]
-            normalize_method = normalize_model[j]
-            click.echo(f'Training {model_type} using {normalize_method} to predict {feature_name}', err=True)
-            raw_data = parse_raw_data(data_filename)
-            normalized = normalize_data(raw_data, method=normalize_method, threshold=normalize_threshold)
-            feature, name_map = parse_feature(metadata_filename, raw_data.index, feature_name=feature_name)
-            train_data, test_data, train_feature, test_feature = split_data(
-                normalized, feature, test_size=test_size
-            )
-            model = train_model(
-                train_data, train_feature, method=model_type, n_estimators=num_estimators, n_neighbours=num_neighbours
-            )   
-            predictions = predict_with_model(model, test_data)
-            click.echo(confusion_matrix(test_feature, predictions.round()))
-            click.echo(classification_report(test_feature, predictions.round()))
-            click.echo(accuracy_score(test_feature, predictions.round()))
+@click.option('--normalize-threshold', default='0.0001',
+              help='Normalization threshold for binary normalization.')
+@click.argument('metadata_file', type=click.File('r'))
+@click.argument('data_file', type=click.File('r'))
+@click.argument('out_file', type=click.File('w'))
+def eval_all(test_size, num_estimators, num_neighbours, feature_name, normalize_threshold,
+             metadata_file, data_file, out_file):
+    """Evaluate all models and all normalizers."""
+    raw_data = parse_raw_data(data_file)
+    feature, name_map = parse_feature(metadata_file, raw_data.index, feature_name=feature_name)
 
-            multiclass_prediction = multi_predict_with_model(model,test_data)
-            click.echo(multiclass_prediction)
+    tbl, seed = {}, randint(0, 1000)
+    for model_name, norm_name in product(MODEL_NAMES, NORMALIZER_NAMES):
+        click.echo(
+            f'Training {model_name} using {norm_name} to predict {feature_name}',
+            err=True
+        )
+        normalized = normalize_data(raw_data, method=norm_name, threshold=normalize_threshold)
+        train_data, test_data, train_feature, test_feature = split_data(
+            normalized, feature, test_size=test_size, seed=seed
+        )
+        model = train_model(
+            train_data, train_feature, method=model_name,
+            n_estimators=num_estimators, n_neighbours=num_neighbours
+        )
+        predictions = predict_with_model(model, test_data).round()
 
-            predict_top_class = predict_top_classes(model, test_data, test_feature)
-            click.echo(predict_top_class)
-            top1.append(predict_top_class[0])
-            top3.append(predict_top_class[1])
-            top5.append(predict_top_class[2])
-            top10.append(predict_top_class[3])
-            model_classifier.append(model_type)
-            model_normalization.append(normalize_method)
-            precision_value = precision_score(test_feature, predictions.round(), average="macro") 
-            recall_value  = recall_score(test_feature, predictions.round(), average="macro") 
-            precision.append((precision_value))
-            recall.append((recall_value))
+        model_results = predict_top_classes(model, test_data, test_feature)
+        model_results.append(precision_score(test_feature, predictions, average="macro"))
+        model_results.append(recall_score(test_feature, predictions, average="macro"))
+        tbl[(model_name, norm_name)] = model_results
 
-    raw_data = {'Classifier': model_classifier,
-                'Preprocessing': model_normalization,
-                'Accuracy': top1,
-                'Top_3_accuracy': top3,
-                'Top_5_accuracy': top5,
-                'Top_10_accuracy': top10,
-                'Precision' : precision,
-                'Recall': recall
-
-                }
-    df = pd.DataFrame(raw_data, columns = ['Classifier', 'Preprocessing', 'Accuracy', 'Top_3_accuracy', 'Top_5_accuracy', 'Top_10_accuracy', 'Precision', 'Recall'])
-    df.to_csv("example_classifier.csv")
+    col_names = [
+        'Classifier',
+        'Preprocessing',
+        'Accuracy',
+        'Top_3_accuracy',
+        'Top_5_accuracy',
+        'Top_10_accuracy',
+        'Precision',
+        'Recall',
+    ]
+    df = pd.DataFrame.from_dict(tbl, columns=col_names, orient='index')
+    df.to_csv(out_file)
 
 
 if __name__ == '__main__':

85809aebbef778d572a6deaa818e079c774493a9
chandrima04 <chandrima.004@google.com>
2019-02-07 10:44:12 -0500

    Cleaned up Class for one and all classifier

--- effet_oppos/cli.py
+++ effet_oppos/cli.py
@@ -18,6 +18,7 @@ from .classification import (
     split_data,
     train_model,
     predict_with_model,
+    multi_predict_with_model,
     predict_top_classes,
 )
 
@@ -31,16 +32,47 @@ def main():
 
 
 @main.command('one')
-def eval_one():
+@click.option('--test-size', default=0.2, help='The relative size of the test data')
+@click.option('--num-estimators', default=20, help='Number of trees in our random forest')
+@click.option('--num-neighbours', default=21, help='Number of clusters in our knn')
+@click.option('--model-name', default='random_forest', help='The model type to train')
+@click.option('--normalize-method', default='standard_scalar', help='Normalization method.')
+@click.option('--feature-name', default='city', help='The feature to predict')
+@click.option('--normalize-threshold', default='0.0001',
+              help='Normalization threshold for binary normalization.')
+@click.argument('metadata_file', type=click.File('r'))
+@click.argument('data_file', type=click.File('r'))
+def eval_one(test_size, num_estimators, num_neighbours, model_name, normalize_method, 
+             feature_name, normalize_threshold, metadata_file, data_file):
     """Train and evaluate a model. Print the model results to stderr."""
-    pass
+    raw_data = parse_raw_data(data_file)
+    seed = randint(0, 1000)
+    feature, name_map = parse_feature(metadata_file, raw_data.index, feature_name=feature_name)
+    
+    click.echo(f'Training {model_name} using {normalize_method} to predict {feature_name}',err=True)
+    
+    normalized = normalize_data(raw_data, method=normalize_method, threshold=normalize_threshold)
+    train_data, test_data, train_feature, test_feature = split_data(
+        normalized, feature, test_size=test_size, seed=seed
+    )
+    model = train_model(
+        train_data, train_feature, method=model_name,
+        n_estimators=num_estimators, n_neighbours=num_neighbours
+    )
+    predictions = predict_with_model(model, test_data).round()
+    click.echo(confusion_matrix(test_feature, predictions.round()))
+    click.echo(classification_report(test_feature, predictions.round()))
+    click.echo(accuracy_score(test_feature, predictions.round()))
+
+    multiclass_prediction = multi_predict_with_model(model,test_data)
+    click.echo(multiclass_prediction)
+
 
 
 @main.command('all')
 @click.option('--test-size', default=0.2, help='The relative size of the test data')
 @click.option('--num-estimators', default=20, help='Number of trees in our random forest')
-@click.option('--num-neighbours', default=21, help='Number of clusters in our knn')
-@click.option('--model-type', default='random_forest', help='The model type to train')
+@click.option('--num-neighbours', default=21, help='Number of clusters in our knn') 
 @click.option('--feature-name', default='city', help='The feature to predict')
 @click.option('--normalize-threshold', default='0.0001',
               help='Normalization threshold for binary normalization.')
@@ -48,7 +80,7 @@ def eval_one():
 @click.argument('data_file', type=click.File('r'))
 @click.argument('out_file', type=click.File('w'))
 def eval_all(test_size, num_estimators, num_neighbours, feature_name, normalize_threshold,
-             metadata_file, data_file, out_file):
+             metadata_file, data_file, out_file):                
     """Evaluate all models and all normalizers."""
     raw_data = parse_raw_data(data_file)
     feature, name_map = parse_feature(metadata_file, raw_data.index, feature_name=feature_name)
@@ -68,16 +100,20 @@ def eval_all(test_size, num_estimators, num_neighbours, feature_name, normalize_
             n_estimators=num_estimators, n_neighbours=num_neighbours
         )
         predictions = predict_with_model(model, test_data).round()
-
+		
         model_results = predict_top_classes(model, test_data, test_feature)
         model_results.append(precision_score(test_feature, predictions, average="macro"))
         model_results.append(recall_score(test_feature, predictions, average="macro"))
-        tbl[(model_name, norm_name)] = model_results
-
+        print(model_results)
+        model_results.insert(0,norm_name);
+        model_results.insert(0,model_name);
+        tbl[str(model_name + ' ' + norm_name)] = model_results
+    print(tbl)
     col_names = [
         'Classifier',
         'Preprocessing',
         'Accuracy',
+        'Top_2_accuracy',
         'Top_3_accuracy',
         'Top_5_accuracy',
         'Top_10_accuracy',

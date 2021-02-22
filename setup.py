67353a759564b29e265bcdf558c7326a44918f2c
Chandrima Bhattacharya <chandrima.004@gmail.com>
2021-02-22 12:01:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ requirements = [
 setuptools.setup(
     name="meta_pred",
     version="0.1.0",
-    url="https://github.com/Chandrima-04/effet_oppos",
+    url="https://github.com/Chandrima-04/meta_pred.git",
     author="Chandrima Bhattacharya",
     author_email="chb4004@med.cornell.edu",
     description="ML to predict metadata from metagenomes",
     packages=setuptools.find_packages(),
-    package_dir={'effet_oppos': 'effet_oppos'},
+    package_dir={'meta_pred': 'meta_pred'},
     entry_points={
         'console_scripts': [
-            'effet_oppos=effet_oppos.cli:main',
+            'meta_pred=meta_pred.cli:main',
         ]
     },
     install_requires=requirements,
@@ -29,5 +29,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
     ],
 )

26d9c5b44ab3fa1ea11096e6b6c08eb511926dd7
Chandrima Bhattacharya <chandrima.004@gmail.com>
2022-06-14 18:53:26 -0400

    Update classification.py
    Updated methodology

--- meta_pred/classification.py
+++ meta_pred/classification.py
@@ -1,13 +1,19 @@
 import numpy as np
+import pandas as pd
+from time import time
 from sklearn.model_selection import (
     train_test_split,
-    KFold,
-    LeaveOneGroupOut
+    StratifiedKFold,
+    LeaveOneGroupOut,
+    cross_val_score,
 )
+from sklearn.metrics import accuracy_score
 from sklearn.ensemble import (
     RandomForestClassifier,
+    RandomForestRegressor,
     ExtraTreesClassifier,
-    AdaBoostClassifier
+    AdaBoostClassifier,
+    VotingClassifier
 )
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
@@ -16,30 +22,22 @@ from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from catboost import CatBoostClassifier
-import lightgbm as lgb
 
 def split_data(data_tbl, features, test_size=0.2, seed=None):
     """Return a tuple of length four with train data, test data, train feature, test feature."""
     return train_test_split(data_tbl, features, test_size=test_size, random_state=seed)
 
-def get_classifier(data_tbl, features, method='random_forest', n_estimators=100, n_neighbours=21, n_components=10, seed=None):
+def get_classifier(data_tbl, features, method='random_forest', n_estimators=1000, n_neighbours=21, seed=None):
     """Fits the model with chosen classifier along with given parameters."""
     if method == "random_forest":
-        classifier = RandomForestClassifier(n_estimators=n_estimators, criterion="entropy", bootstrap=True, warm_start=True, random_state=seed)
+        classifier = RandomForestClassifier(n_estimators=n_estimators, criterion="entropy", bootstrap=True, random_state=seed)
     elif method == "decision_tree":
         classifier = DecisionTreeClassifier(random_state=seed)
     elif method == "extra_tree":
         classifier = ExtraTreesClassifier(n_estimators=n_estimators, criterion='entropy', random_state=seed)
     elif method == "adaboost":
         classifier = AdaBoostClassifier(n_estimators=n_estimators, learning_rate=4)
-    elif method == "cat":
-        classifier = CatBoostClassifier(n_estimators=n_estimators, loss_function='MultiClass', eval_metric='Accuracy',
-                     leaf_estimation_method='Newton', learning_rate=0.1, random_strength=0.1, random_seed=seed)
-    elif method == "lgb":
-        classifier = lgb.LGBMClassifier(objective="multiclass", n_estimators=n_estimators, extra_trees=True, learning_rate=0.01, seed=seed)
     elif method == "gaussian":
         kernel_gpc = 1.0 * RBF(1.0)
         classifier = GaussianProcessClassifier(kernel=kernel_gpc, random_state=seed)
@@ -54,49 +52,54 @@ def get_classifier(data_tbl, features, method='random_forest', n_estimators=100,
             gamma='scale', decision_function_shape='ovo', kernel="rbf", probability=True
         )
     elif method == "logistic_regression":
-        classifier = LogisticRegression(solver='lbfgs', C=1e5, max_iter= 10000)
-    elif method == "neural_network":
-        classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), max_iter= 10000)
+        classifier = LogisticRegression(solver='lbfgs', C=1e5, max_iter= 1000000)
     elif method == "LDA":
-        classifier = LinearDiscriminantAnalysis()
+        classifier = LinearDiscriminantAnalysis(solver='svd')
+    elif method == "mixed":
+    	clf1 = LogisticRegression(solver='lbfgs', C=1e5, max_iter= 1000000)
+    	clf2 = RandomForestClassifier(n_estimators=n_estimators, criterion="entropy", bootstrap=True, warm_start=True, random_state=seed)
+    	clf3 = svm.SVC(kernel='linear', probability=True)
+    	classifier = VotingClassifier(estimators=[('lr', clf1), ('rf', clf2), ('lsvc', clf3)], voting='soft')
     return classifier
 
 
-def k_fold_crossvalid(data_tbl, features, method='random_forest', n_estimators=100, n_neighbours=21, n_components=10, k_fold=10, seed=None):
+def k_fold_crossvalid(data_tbl, features, method='random_forest', n_estimators=1000, n_neighbours=21, k_fold=10, seed=None):
     """Return a model for saving after training using k-fold cross-validation."""
     scores = []
     classifier_score = 0
-    classifier = get_classifier(data_tbl, features, method=method, n_estimators=n_estimators, n_neighbours=n_neighbours, n_components=n_components, seed=None)
-    cv = KFold(n_splits=k_fold, shuffle=False)
-    for train_index, test_index in cv.split(data_tbl):
+    classifier = get_classifier(data_tbl, features, method=method, n_estimators=n_estimators, n_neighbours=n_neighbours, seed=None)
+    cv = StratifiedKFold(n_splits=k_fold, shuffle=False)
+    for train_index, test_index in cv.split(data_tbl, features):
         X_train, X_test = data_tbl[train_index], data_tbl[test_index]
         y_train, y_test = features[train_index], features[test_index]
         classifier.fit(X_train, y_train)
         scores.append(classifier.score(X_test, y_test))
         if classifier_score < classifier.score(X_test, y_test):
-             X_train_best, X_test_best, y_train_best, y_test_best = X_train, X_test, y_train, y_test
-    return (classifier.fit(X_train_best, y_train_best), np.mean(scores), np.std(scores))
+            classifier_score = classifier.score(X_test, y_test)
+    return np.mean(scores), np.std(scores), classifier_score
 
-def leave_one_group_out(data_tbl, features, group_name, method='random_forest', n_estimators=100, n_neighbours=21, n_components=10, seed=None):
+def leave_one_group_out(data_tbl, features, group_name, method='random_forest', n_estimators=1000, n_neighbours=21, seed=None):
     scores = []
     classifier_score = 0
-    classifier = get_classifier(data_tbl, features, method=method, n_estimators=n_estimators, n_neighbours=n_neighbours, n_components=n_components, seed=None)
+    classifier = get_classifier(data_tbl, features, method=method, n_estimators=n_estimators, n_neighbours=n_neighbours, seed=None)
     leave_one_group = LeaveOneGroupOut()
     for train_index, test_index in leave_one_group.split(data_tbl, y=features, groups=group_name):
         X_train, X_test = data_tbl[train_index], data_tbl[test_index]
         y_train, y_test = features[train_index], features[test_index]
+        print (X_train.shape, X_test.shape, y_train.shape, y_test.shape)
         classifier.fit(X_train, y_train)
         scores.append(classifier.score(X_test, y_test))
         if classifier_score < classifier.score(X_test, y_test):
-             X_train_best, X_test_best, y_train_best, y_test_best = X_train, X_test, y_train, y_test
-    return classifier.fit(X_train_best, y_train_best), np.mean(scores), np.std(scores), X_test, y_test
+            classifier_score = classifier.score(X_test, y_test)
+    return np.mean(scores), np.std(scores), classifier_score
 
-
-def train_model(data_tbl, features, method='random_forest', n_estimators=100, n_neighbours=21, n_components=10, seed=None):
+def train_model(data_tbl, features, method='random_forest', n_estimators=1000, n_neighbours=21, seed=None):
     """Return a trained model to predict features from data."""
-    classifier = get_classifier(data_tbl, features, method=method, n_estimators=n_estimators, n_neighbours=n_neighbours, n_components=n_components, seed=None)
+    t0 = time()
+    print (type(data_tbl), type(features), data_tbl.shape, features.shape)
+    classifier = get_classifier(data_tbl, features, method=method, n_estimators=n_estimators, n_neighbours=n_neighbours, seed=None)
     classifier.fit(data_tbl, features)
-    return classifier
+    return classifier, (time() - t0)
 
 
 def predict_with_model(model, data_tbl):
